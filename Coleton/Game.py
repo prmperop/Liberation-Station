e1b2045b86a239463107d727c995dadf859c836f
colejagdtiger <coleton.kotch@gmail.com>
2016-11-10 11:51:36 -0700

    Add files via upload

---
+++ Coleton/Game.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.init()
+
+class hero(object):
+    def __init__(self,armor,speed,punch,AGmod,Gmod,resist):
+        self.armor = armor
+        self.speed = speed
+        seld.punch = punch
+        self.AGmod = AGmod
+        self.Gmod = Gmod
+        Self.resist = resist
