0a1764d59476de1139b7ebf264dfd6526fb19301
colejagdtiger <coleton.kotch@gmail.com>
2016-11-10 12:02:08 -0700

    Delete Game.py

--- Coleton/Game.py
+++
@@ -1,9 +0,0 @@
-import pygame
-pygame.init()
-
-class Enemy(object):
-    def __init__(self,armor,speed,resist):
-        self.armor = armor
-        self.speed = speed
-        self.resist = resist
-# resist = spell resist
