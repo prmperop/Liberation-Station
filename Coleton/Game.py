befd4d0fcaadc70bcaf2d32a66be48a6b98caa45
colejagdtiger <coleton.kotch@gmail.com>
2016-11-10 11:45:25 -0700

    Update Game.py

--- Coleton/Game.py
+++ Coleton/Game.py
@@ -2,10 +2,15 @@ import pygame
 pygame.init()
 
 class hero(object):
-    def __init__(self,armor,speed,punch,AGmod,Gmod,resist):
+    def __init__(self,armor,speed,punch,ABmod,Bmod,Mmod,AMmod,spell,resist):
         self.armor = armor
         self.speed = speed
-        seld.punch = punch
-        self.AGmod = AGmod
-        self.Gmod = Gmod
-        Self.resist = resist
+        self.punch = punch
+        self.ARmod = ARmod
+        self.Rmod = Rmod
+        self.AMmod = Mmod
+        self.Mmod = Mmod
+        self.resist = resist
+"""ARmod = alien ranged mod, Rmod = Ranged mod, AMmod = alien melee mod, Mmod = melee mod,
+spell is if they can cast spells or not resist = spell resistance,"""
+#this is an idea I have on a character class
